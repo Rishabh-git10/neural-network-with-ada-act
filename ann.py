b5c793dd6c6e5115c68144957ba33bbda726a5f4
Rishabh <romantimeless@gmail.com>
2023-07-22 15:23:23 +0530

    defined fun to store backprop error in neurons

--- ann.py
+++ ann.py
@@ -99,3 +99,22 @@ def forward_propagate(network, row, k0, k1):
 def transfer_derivative(output, k1):
     return k1
 
+# Backpropagate error and store in neurons
+def backward_propagate_error(network, expected, k1):
+    for i in reversed(range(len(network))):
+        layer = network[i]
+        errors = list()
+        if i != len(network)-1:
+            for j in range(len(layer)):
+                error = 0.0
+                for neuron in network[i + 1]:
+                    error += (neuron['weights'][j] * neuron['delta'])
+                errors.append(error)
+        else:
+            for j in range(len(layer)):
+                neuron = layer[j]
+                errors.append(expected[j] - neuron['output'])
+        for j in range(len(layer)):
+            neuron = layer[j]
+            neuron['delta'] = errors[j] * transfer_derivative(neuron['output'], k1)
+
